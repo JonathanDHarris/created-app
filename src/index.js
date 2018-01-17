b5e9f1fbe21a3725df61cde414121820d2d55bd5
Jon Harris <jharris@scottlogic.com>
2018-01-17 09:24:55 +0000

    Initial commit after create react app and following https://medium.com/@notrab/getting-started-with-create-react-app-redux-react-router-redux-thunk-d6a19259f71f

---
+++ src/index.js
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
+import App from './containers/app'
+
+import './index.css'
+
+const target = document.querySelector('#root')
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+		<App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
+)
+
+
+/*
+ReactDOM.render(
+	<ConnectedRouter history={history}>
+	<div>hello</div>
+	</ConnectedRouter>,
+	target
+)
+*/

3e6c9c44dc2969c2ff7587e75708115ddc6aeb3f
Jon Harris <jharris@scottlogic.com>
2018-01-17 09:43:16 +0000

    Tests for actions

--- src/modules/counter.test.js
+++ src/modules/counter.test.js
@@ -1,19 +1,60 @@
-import { increment, INCREMENT, INCREMENT_REQUESTED } from './counter.js'
+import reducer, { increment, INCREMENT, INCREMENT_REQUESTED } from './counter.js'
 
-test('Increment increases counter by one', () => {
-	const mockDispatch = jest.fn()
-	
-	const expected1 = {
+	const incrementRequestedAction = {
 		type: INCREMENT_REQUESTED
 	}
+
 	
-	const expected2 = {
+	const incrementAction = {
 		type: INCREMENT
 	}
+	
+test('Increment dispatches with INCREMENT_REQUESTED and then INCREMENT', () => {
+	const mockDispatch = jest.fn()
+	
+
+	
+
 	
 	const result = increment()
 	
 	result(mockDispatch)
-	expect(mockDispatch.mock.calls[0]).toEqual([expected1])
-	expect(mockDispatch.mock.calls[1]).toEqual([expected2])
+	expect(mockDispatch.mock.calls[0]).toEqual([incrementRequestedAction])
+	expect(mockDispatch.mock.calls[1]).toEqual([incrementAction])
+})
+
+test('Increment_Requested sets isIncrementing to true', () => {
+	const initialState = {
+	  count: 0,
+	  isIncrementing: false,
+	  isDecrementing: false
+	}
+		
+	const newState = reducer(initialState, incrementRequestedAction)
+	
+	expect(newState.isIncrementing).toEqual(true)
+})
+
+test('Increment increases counter by one', () => {
+	const initialState = {
+	  count: 0,
+	  isIncrementing: false,
+	  isDecrementing: false
+	}
+		
+	const newState = reducer(initialState, incrementAction)
+	
+	expect(newState.count).toEqual(1)
+})
+
+test('Increment sets isIncrementing to false', () => {
+	const initialState = {
+	  count: 0,
+	  isIncrementing: true,
+	  isDecrementing: false
+	}
+		
+	const newState = reducer(initialState, incrementAction)
+	
+	expect(newState.isIncrementing).toEqual(false)
 })
